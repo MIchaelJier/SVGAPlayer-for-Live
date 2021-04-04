5083ef9d434809f06936c5bd166e8c2ccd5495bc
MichaelJier <chem908430737@icloud.com>
2021-04-04 22:54:16 +0800

    update 1. 调整baseOptions，为SVGAPlayer-Web-Lite Player.set的参数 2. 统一事件$on

--- README.md
+++ README.md
@@ -12,17 +12,34 @@ yarn add @gdyfe/svgaplayer-for-live -D
 // npm
 import websocketHeartbeat from '@gdyfe/svgaplayer-for-live'
 new SVGAPlayer({
-  target: string | HTMLCanvasElement 
-  db?: boolean // boolean  是否开启IndexedDB 
-  preDown?: number // 预加载个数 
+    // option
+    target: string | HTMLCanvasElement 
+    db?: boolean // boolean  是否开启IndexedDB 
+    preDown?: number // number 预加载个数 
+  },
+  { 
+    // baseOption 参考 SVGAPlayer-Web-Lite => Player.set({ 参数 })
+    loop: 1,  
+    cacheFrames: false,
+    intersectionObserverRender: true,
   })
 ```
-## HOOK
-| 选项名          | 说明 | 参数                 
-| :- | :- | :- |
-| startCallback | 动画开始回调 | type SVGAElementItem (返回当前播放项)|
-| endCallback | 动画结束回调 | / |
-
+## Event
+```javascript
+SVGAPlayer 
+    // 开始动画事件回调
+    .$on('start', (SVGAElementItem: 当前正在播放项) => console.log('event start'))
+    // 暂停动画事件回调
+    .$on('pause', (SVGAElementItem: 当前正在播放项) => console.log('event pause'))
+    // 停止动画事件回调
+    .$on('stop', (SVGAElementItem: 当前正在播放项) => console.log('event stop'))
+    // 动画结束事件回调
+    .$on('end', (SVGAElementItem: 当前正在播放项) => console.log('event end'))
+    // 清空动画事件回调
+    .$on('clear', (SVGAElementItem: 当前正在播放项) => console.log('event clear')) 
+    // 动画播放中事件回调
+    .$on('process', (SVGAElementItem: 当前正在播放项) => console.log('event process', player.progress))
+```
 ## 功能
 - [x]  队列执行动画
 - [x]  IndexedDB 持久化缓存
--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable no-empty-function */
 /* eslint-disable no-undef */
 import Queue from './queue'
-import SVAG, { Downloader, EVENT_TYPES, VideoEntity } from 'svga.lite'
+import SVAG, {
+  Downloader,
+  EVENT_TYPES,
+  VideoEntity,
+  options as playOptions,
+} from 'svga.lite'
 import {
   SVGAElementArr,
   SVGAElementItem,
@@ -10,9 +17,9 @@ import {
 import DB from './db'
 
 class PlayerQueue extends Queue {
-  private player: SVAG.Player | null = null
-  private parser: SVAG.Parser | null = null
-  private downloader: SVAG.Downloader | null = null
+  public player: SVAG.Player | null = null
+  public parser: SVAG.Parser | null = null
+  public downloader: SVAG.Downloader | null = null
   private db: DB | null = null
   private playing = false
   private playedSum = 0
@@ -21,12 +28,18 @@ class PlayerQueue extends Queue {
     useDB: true,
     preDown: 0,
   }
+  private baseOptions: playOptions = {
+    loop: 1,
+    // 开启后对已绘制的帧进行缓存，提升重复播放动画性能
+    cacheFrames: false,
+    // 检测动画容器是否处于视窗内，若处于视窗外，停止描绘渲染帧避免造成资源消耗
+    intersectionObserverRender: true,
+  }
 
-  public startCallback: ((now: SVGAElementItem) => void) | null = null
-  public endCallback: ((now: SVGAElementItem) => void) | null = null
-  constructor(options: SVGAoptions) {
+  constructor(options: SVGAoptions, baseOptions: playOptions) {
     super()
     Object.assign(this.options, options)
+    Object.assign(this.baseOptions, baseOptions)
     this.init(options?.target)
   }
   /**
@@ -43,6 +56,38 @@ class PlayerQueue extends Queue {
       console.error(error)
     }
   }
+
+  public $on(
+    eventName: EVENT_TYPES,
+    execFunction: (element: SVGAElementItem | undefined) => void
+  ): PlayerQueue | undefined {
+    // this.$onEvent[eventName] = execFunction
+    if (!this.player) return
+    switch (eventName) {
+      case 'start':
+        this.player.$on('start' as EVENT_TYPES.START, () => {
+          this.playing = true
+          execFunction && execFunction(this.front())
+        })
+        break
+      case 'end':
+        this.player.$on('end' as EVENT_TYPES.END, () => {
+          this.playing = false
+          execFunction && execFunction(this.front())
+          this.playedSum++
+          this.dequeue()
+          this.play()
+        })
+        break
+      default:
+        this.player.$on(eventName, () => {
+          execFunction(this.front())
+        })
+        break
+    }
+    return this
+  }
+
   /**
    * play 队列播放动画
    */
@@ -77,33 +122,15 @@ class PlayerQueue extends Queue {
         this.db &&
         (await (this.db as any).insert(this.front().value, svgaData))
     }
-    player.set({
-      loop: 1,
-      // 开启后对已绘制的帧进行缓存，提升重复播放动画性能
-      cacheFrames: false,
-      // 检测动画容器是否处于视窗内，若处于视窗外，停止描绘渲染帧避免造成资源消耗
-      intersectionObserverRender: true,
-    })
+    player.set(this.baseOptions)
     await player.mount(svgaData)
-    player.$on('end' as EVENT_TYPES.END, () => {
-      downloader.cancel()
-      this.playing = false
-      this.endCallback && this.endCallback(this.front())
-      this.playedSum++
-      this.dequeue()
-      this.play()
-    })
-    player.$on('start' as EVENT_TYPES.START, () => {
-      this.playing = true
-      this.startCallback && this.startCallback(this.front())
-    })
     player.start()
   }
   /**
    * svga 预加载 默认加载后面的1个
    * @param num 预加载个数
    */
-  public async preDown(num = 1): Promise<void> {
+  private async preDown(num = 1): Promise<void> {
     const next = this.slice(1, num + 1)
     const lastPlayedSum = this.playedSum
     next.forEach(async (item: SVGAElementItem, index: number) => {
